13b122591ee2f6ec3f52a345be712722495676d2
Dusty Phillips <dusty@linux.ca>
2012-11-29 20:37:02 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name="gitifyhg",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        'path.py',
+        'sh'
+    ]
+)
