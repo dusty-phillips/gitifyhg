d73a15720cfe792c65d1485f2fdb0038683e6098
Dusty Phillips <dusty@linux.ca>
2012-11-30 08:10:29 -0700

    metainformation

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup, find_packages
 setup(
     name="gitifyhg",
+    author="Dusty Phillips",
+    author_email="dusty@buchuki.com",
+    url="https://github.com/buchuki/gitifyhg",
     version="0.1",
     packages=find_packages(),
     install_requires=[
