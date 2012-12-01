8d3fbe2a166a4247194a48c5dbf3b6ff3e0972a0
Dusty Phillips <dusty@linux.ca>
2012-12-01 10:50:24 -0700

    Check more things when gitify is called

--- test/test_gitifyhg.py
+++ test/test_gitifyhg.py
@@ -5,6 +5,11 @@ import sh
 
 
 def pytest_funcarg__hg_repo(request):
+    '''funcarg hg_repo that creates an hg repository in a temporary directory
+    and clones it. This allows testing of pulls and pushes between
+    repositories.
+
+    :param request: The pytest_funcarg request object'''
     tmpdir = p(str(request.getfuncargvalue('tmpdir')))
     hg_base = tmpdir.joinpath('hg_base')  # an hg repo to clone from
     hg_base.mkdir()  # could be on the above line if jaraco would release a bugfix
@@ -23,10 +28,15 @@ def pytest_funcarg__hg_repo(request):
 
 
 def test_gitify(hg_repo):
+    '''Ensure that gitifyhg has done it's job.'''
     gitifyhg()
 
     assert hg_repo.joinpath('.git').isdir()
     assert sh.git.alias().stdout == (
         b'hgpull = !gitifyhg hgpull\nhgpush = !gitifyhg hgpush\n')
-    print(sh.git.log(pretty='oneline'))
+    # There is one commit in both hg and git
     assert sh.git.log(pretty='oneline').stdout.count(b'\n') == 1
+    assert sh.grep(sh.hg.log(), 'changeset:').stdout.count(b'\n') == 1
+    # both hg and git have nothing unexpected in the working directory
+    assert len(sh.hg.status().stdout) == 0
+    assert len(sh.git.status(short=True).stdout) == 0
