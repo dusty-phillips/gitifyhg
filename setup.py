5e23978bc17c915554230cb53264384a6c069ef6
Dusty Phillips <dusty@linux.ca>
2013-03-06 15:29:16 -0700

    Prepare to release 0.7.2, which seems to have fixed the tags issue completely.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     author_email="dusty@buchuki.com",
     url="https://github.com/buchuki/gitifyhg",
     description="Use git as client for hg repos",
-    version="0.7.1",
+    version="0.7.2",
     py_modules=["gitifyhg"],
     install_requires=[
         'path.py>=2.5',
