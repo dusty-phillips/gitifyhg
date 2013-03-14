bd3716612d6508a0e790d97157fb09f1bf47d2b3
Dusty Phillips <dusty@linux.ca>
2013-03-14 16:06:24 -0600

    bump version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     author_email="dusty@buchuki.com",
     url="https://github.com/buchuki/gitifyhg",
     description="Use git as client for hg repos",
-    version="0.8",
+    version="0.8.1",
     packages=['gitifyhg'],
     install_requires=[
         'path.py>=2.5',
