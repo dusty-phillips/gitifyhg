7c6ebe3f547af03024705b6928b74f4318a1f230
Justin Dalrymple <justin.s.dalrymple@gmail.com>
2017-01-15 11:53:02 -0500

    Tests pass locally

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     packages=['gitifyhg'],
     install_requires=[
         'path.py>=2.5',
-        'Mercurial>=2.5',
+        'Mercurial>=2.5, <4.0.2',
     ],
     entry_points={
         'console_scripts': [
